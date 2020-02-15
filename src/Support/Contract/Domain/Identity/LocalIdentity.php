b253d772bfe170bdadb09fe7ee0ca056a4c3498e
steph bug <plexikon@protonmail.com>
2020-02-15 16:27:27 +0100

    fix missing interface

--- src/Support/Contract/Domain/Identity/LocalIdentity.php
+++ src/Support/Contract/Domain/Identity/LocalIdentity.php
@@ -2,7 +2,9 @@
 
 namespace Plexikon\Auth\Support\Contract\Domain\Identity;
 
+use Plexikon\Auth\Support\Contract\Value\Credential\EncodedCredential;
+
 interface LocalIdentity extends Identity
 {
-    public function getPassword(): EncodedCredentials;
+    public function getPassword(): EncodedCredential;
 }
